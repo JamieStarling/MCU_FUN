3d9d031e661704c8b1be051a7e075cf156a2d740
JamieStarling <52940035+JamieStarling@users.noreply.github.com>
2020-11-04 16:01:46 -0500

    Clean up folders

---
+++ Microchip/PIC10F322/Arduino_API/template.c
@@ -0,0 +1,18 @@
+*
+ * File:   template.c
+ * Author: Jamie
+ *
+ * Created on November 4, 2020, 4:00 PM
+ * Template for use the PIC10F322 Arduino API
+ */
+
+#include "Arduino_PIC10F322/arduino.h"
+
+
+void setup(void){
+      
+}
+
+void loop (void){    
+  
+}
